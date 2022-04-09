50edca29f9ba1629b63184f05e24a91273a60788
Anisbeflo <abejarano0@ucol.mx>
2022-04-09 15:37:33 -0500

    Added filter for actives and inactive rockets

--- src/components/rocket/component.tsx
+++ src/components/rocket/component.tsx
@@ -2,8 +2,15 @@ import React, { ChangeEvent, FC, useEffect, useState } from "react";
 import { getRockets } from "../../services/spacex/service";
 import "../../styles.css";
 
+const options = [
+  'All',
+  'Active',
+  'Inactive',
+]
+
 export const Rocket: FC = ()=>{
   const [Rocket,setRocket] = useState<any>(undefined);
+  const [filter,setFilter] = useState<any>("all");
 
   useEffect(()=>{
     const loadRockets = async ()=>{
@@ -13,20 +20,39 @@ export const Rocket: FC = ()=>{
     loadRockets();
   },[]);
 
-  console.log(Rocket);
+  const filterByActive = (event: ChangeEvent<HTMLSelectElement>) => {
+    if (event.target.value==="active")
+      setFilter(true);
+    else if (event.target.value==="inactive")
+      setFilter(false);
+    else setFilter("all");
+    console.log(filter)
+  };
+
   return(
     <>
       <div className="title">Rockets</div>
+      <p>Show: </p>
+      <select onChange={filterByActive}>
+        <option value="all">All</option>
+        <option value="active">Active</option>
+        <option value="inactive">Inactive</option>
+      </select>
       {Rocket!== undefined ? (
         <div className="card-group">
           {Rocket.map((rocketIndex:any)=>(
+            <>
+            {rocketIndex.active===filter||filter==="all" ? (
             <div className="card">
-              <p><b>Name:</b> {rocketIndex.name}</p>
-              <p className="status"><b>Active:</b> {rocketIndex.active ? <div className="active"/> : <div className="inactive"/>}</p>
-              <p><b>First Flight:</b> {rocketIndex.first_flight}</p>
-              <p><b>Description:</b> {rocketIndex.description}</p>
-              <br/>
-            </div>
+            <p><b>Name:</b> {rocketIndex.name}</p>
+            <p className="status"><b>Active:</b> {rocketIndex.active ? <div className="active"/> : <div className="inactive"/>}</p>
+            <p><b>First Flight:</b> {rocketIndex.first_flight}</p>
+            <p><b>Description:</b> {rocketIndex.description}</p>
+            <br/>
+          </div>)
+          : null
+          }</>
+
           ))}
         </div>
       ) : null}
